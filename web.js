c0f099055e8465eb7d1c7710e917c87e28e5a0c1
John Smith <example@stanford.edu>
2013-07-10 19:33:12 +0000

    modify web.js: read index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs')
 
-var app = express.createServer(express.logger());
+app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFile('./index.html', function (err, data) {
+   if (err) throw err;
+   var buffer = new Buffer(data, 'utf-8');
+   response.send(buffer.toString('utf-8'));
+ });  
 });
 
 var port = process.env.PORT || 5000;
